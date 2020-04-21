375484cb86397964aabbe9ef24af85933838b5e0
Ken Chen <kenchen@microsoft.com>
2020-04-21 13:39:50 +0800

    update azure-websocket.js

--- websocket/azure-websocket.js
+++ websocket/azure-websocket.js
@@ -1,9 +1,5 @@
-async function processWithTrace(func) {
-  let start = new Date();
-  let message = await func();
-  let end = new Date();
-  traces.push(`[${start} +${end - start}ms] ${message}`);
-}
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 function parseConnectionString(connString) {
   const pattern = /^Endpoint=(.*);AccessKey=(.*);Version=1.0;$/;
@@ -22,16 +18,19 @@ function generateToken(url, key) {
   });
 }
 
-async function invokeService(connectionInfo, method, path, obj) {
+async function invokeService(connectionInfo, method, path, name, data) {
   console.log(`${method} ${path}`);
   let url = `${connectionInfo.endpoint}/ws/api/v1/${path}`;
   let headers = {
     'Authorization': generateToken(url, connectionInfo.accessKey)
   };
   let payload = null;
-  if (obj) {
+  if (name) {
     headers['Content-Type'] = 'text/plain';
-    payload = JSON.stringify(obj);
+    payload = JSON.stringify({
+      name: name,
+      data: data
+    });
   }
   try {
     let res;
@@ -45,109 +44,72 @@ async function invokeService(connectionInfo, method, path, obj) {
   }
 }
 
-function AzureWebsocket(connString, hubName) {
+function AzureWebsocket(connString, hubName, handlers) {
   this._connectionInfo = parseConnectionString(connString);
   this._hubName = hubName;
+  this._handlers = handlers;
 }
 
-AzureWebSocket.prototype.send = async function (name, data) {
-  await invokeService(this._connectionInfo, 'post', `hubs/${this._hubName}`, {
-    name: name,
-    data: data
-  });
+AzureWebsocket.prototype.parseEvent = function (headers) {
+  let query = {};
+  for (let p of new URLSearchParams(headers['x-asrs-client-query']).entries()) {
+    query[p[0]] = p[1];
+  }
+  return {
+    connectionId: headers['x-asrs-connection-id'],
+    userId: headers['x-asrs-user-id'],
+    query: query,
+    socket: this
+  };
 };
-AzureWebSocket.prototype.sendToConneciton = async function (connectionId, name, data) {
-  await invokeService(this._connectionInfo, 'post', `hubs/${this._hubName}/connections/${connectionId}`, {
-    name: name,
-    data: data
-  });
+
+AzureWebsocket.prototype.send = async function (name, data) {
+  await invokeService(this._connectionInfo, 'post', `hubs/${this._hubName}`, name, data);
 };
-AzureWebSocket.prototype.addToGroup = async function (connectionId, groupName) {
+
+AzureWebsocket.prototype.sendToConneciton = async function (connectionId, name, data) {
+  await invokeService(this._connectionInfo, 'post', `hubs/${this._hubName}/connections/${connectionId}`, name, data);
+};
+
+AzureWebsocket.prototype.addToGroup = async function (connectionId, groupName) {
   await invokeService(this._connectionInfo, 'put', `hubs/${this._hubName}/groups/${groupName}/connections/${connectionId}`);
 };
-AzureWebSocket.prototype.sendToGroup = async function (groupName, name, data) {
-  await invokeService(this._connectionInfo, 'post', `hubs/${this._hubName}/groups/${groupName}`, {
-    name: name,
-    data: data
-  });
+
+AzureWebsocket.prototype.sendToGroup = async function (groupName, name, data) {
+  await invokeService(this._connectionInfo, 'post', `hubs/${this._hubName}/groups/${groupName}`, name, data);
 };
+
 AzureWebsocket.prototype.sendToUser = async function (userId, name, data) {
-  await invokeService(this._connectionInfo, 'post', `hubs/${this._hubName}/users/${userId}`, {
-    name: name,
-    data: data
-  });
+  await invokeService(this._connectionInfo, 'post', `hubs/${this._hubName}/users/${userId}`, name, data);
 };
 
-module.exports = {
-  create: (connString, hubName) => new AzureWebSocket(connString, hubName)
+AzureWebsocket.prototype.onConnected = async function (headers) {
+  let res = {
+    headers: {}
+  };
+  let context = this.parseEvent(headers);
+  res.body = await this._handlers.onConnected(context);
+  if (context.userId) res.headers['x-asrs-user-id'] = context.userId;
+  console.log(`${context.connectionId} connected`);
+  return res;
 };
 
-function initialize(app, hubName, handlers, connectionString) {
-  function parseEvent(req) {
-    let query = {};
-    for (let p of new URLSearchParams(req.header('x-asrs-client-query')).entries()) {
-      query[p[0]] = p[1];
-    }
-    return {
-      connectionId: req.header('x-asrs-connection-id'),
-      userId: req.header('x-asrs-user-id'),
-      query: query,
-      send: send,
-      sendToConnection: sendToConneciton,
-      sendToGroup: sendToGroup,
-      sendToUser: sendToUser,
-      addToGroup: addToGroup
-    };
-  }
+AzureWebsocket.prototype.onDisconnected = async function (headers) {
+  let context = this.parseEvent(headers);
+  await this._handlers.onDisconnected(context);
+  console.log(`${context.connectionId} disconnected`);
+};
 
-  const connectionInfo = parseConnectionString(connectionString);
-  app
-    .get(`/${hubName}`, (req, res) => {
-      res.send(`ws${connectionInfo.endpoint.slice(4)}/ws/client/hubs/${hubName}`);
-    })
-    .post('/background/upload', async (req, res) => {
-      diagram.background = {
-        id: Math.random().toString(36).substr(2, 8),
-        data: req.files['file'].data,
-        contentType: req.files['file'].mimetype
-      };
-      await send('backgroundUpdated', diagram.background.id)
-      res.send();
-    })
-    .get('/background/:id', (req, res) => {
-      if (diagram.background && diagram.background.id === req.params.id) {
-        res.type(diagram.background.contentType);
-        res.send(diagram.background.data);
-      } else res.status(404).send();
-    })
-    .post(`/${hubName}/connect`, async (req, res) => {
-      await processWithTrace(async () => {
-        let context = parseEvent(req);
-        let content = await handlers.onConnected(context);
-        if (context.userId) res.header('x-asrs-user-id', context.userId);
-        if (content) res.send(content);
-        else res.status(204).send();
-        return `${context.connectionId} connected`;
-      });
-    })
-    .post(`/${hubName}/disconnect`, async (req, res) => {
-      await processWithTrace(async () => {
-        let context = parseEvent(req);
-        await handlers.onDisconnected(context);
-        res.send();
-        return `${context.connectionId} disconnected`;
-      });
-    })
-    .post(`/${hubName}/message`, async (req, res) => {
-      await processWithTrace(async () => {
-        let context = parseEvent(req);
-        let obj = JSON.parse(req.body);
-        if (handlers[obj.name]) await handlers[obj.name](context, obj.data);
-        res.send();
-        return `received ${obj.name} from ${context.connectionId}`;
-      });
-    })
-    .get('/debug', (req, res) => {
-      res.send(JSON.stringify(traces));
-    });
-}
+AzureWebsocket.prototype.onMessage = async function (headers, body) {
+  let context = this.parseEvent(headers);
+  let obj = JSON.parse(body);
+  console.log(`${obj.name} received`);
+  if (this._handlers[obj.name]) return await this._handlers[obj.name](context, obj.data);
+  else throw `unrecognized message: ${body}`;
+};
+
+AzureWebsocket.prototype.getEndpoint = function () {
+  return `ws${this._connectionInfo.endpoint.slice(4)}/ws/client/hubs/${this._hubName}`;
+};
+
+module.exports = AzureWebsocket;
